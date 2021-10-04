853a3ac02a25bc2734646ed0b087e0b9f467cad6
unknown <xuanphuchb@gmail.com>
2021-10-04 19:18:44 +0700

    nxp

---
+++ src/main/java/com/freelancer/configuration/TestMain.java
@@ -0,0 +1,9 @@
+package com.freelancer.configuration;
+
+public class TestMain {
+	public static void main(String helloWorld[]) {
+		Test test = new Test();
+		test.a = "hihi";
+		System.out.println(test.a);
+	}
+}
