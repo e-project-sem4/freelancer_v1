d61493cce61bc9979de19cda8fefdab551511240
Windows 10 <trinhlhth1908021@fpt.edu.vn>
2021-10-10 23:35:13 +0700

    fix crud

--- src/main/java/com/freelancer/repository/JobRepository.java
+++ src/main/java/com/freelancer/repository/JobRepository.java
@@ -1,9 +1,24 @@
 package com.freelancer.repository;
 
+import com.freelancer.model.ExpectedDuration;
 import com.freelancer.model.Job;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
+    @Query(value = "SELECT a FROM Job a WHERE (UPPER(a.name) like UPPER(CONCAT('%', :keysearch,'%'))) "
+            + "ORDER BY a.id")
+    List<Job> searchJob(@Param("keysearch") String keysearch, Pageable pageable);
+
+    @Query(value = "SELECT count(a) FROM Job a WHERE (UPPER(a.name) like UPPER(CONCAT('%', :keysearch,'%'))) "
+            + "ORDER BY a.id")
+    Long countJob(@Param("keysearch") String keysearch);
+
 }
