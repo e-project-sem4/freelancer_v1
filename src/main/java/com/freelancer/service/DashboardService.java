00e12aa61e52b76a915f54b451caa218f9151603
Windows 10 <trinhlhth1908021@fpt.edu.vn>
2021-10-24 15:38:16 +0700

    giao dich

--- src/main/java/com/freelancer/service/DashboardService.java
+++ src/main/java/com/freelancer/service/DashboardService.java
@@ -2,12 +2,9 @@ package com.freelancer.service;
 
 import java.util.HashMap;
 
+import com.freelancer.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.freelancer.repository.UserBusinessRepository;
-import com.freelancer.repository.UserFreelancerRepository;
-import com.freelancer.repository.UserRepository;
 
 @Service
 public class DashboardService {
@@ -21,14 +18,21 @@ public class DashboardService {
     @Autowired
     private UserFreelancerRepository userFreelancerRepository;
 
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private OtherSkillRepository otherSkillRepository;
+
     public HashMap<Integer, Long> getCountDashboard() {
         HashMap<Integer, Long> count = new HashMap<Integer, Long>();
         Long countUser = userRepository.count();
         Long countFreelancer = userFreelancerRepository.count();
         Long countBusiness = userBusinessRepository.count();
+        Long countJob = jobRepository.count();
         count.put(1,countUser);
         count.put(2,countFreelancer);
         count.put(3,countBusiness);
+        count.put(4,countJob);
         return count;
     }
 }
