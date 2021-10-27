6b304c3d0677a9807c3ee91a0b5f1a5e3d8d1838
unknown <xuanphuchb@gmail.com>
2021-10-27 14:05:15 +0700

    ok

--- src/main/java/com/freelancer/sendmail/FormSendMail.java
+++ src/main/java/com/freelancer/sendmail/FormSendMail.java
@@ -7,14 +7,25 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FormSendMail{
-    @Autowired
-    private JavaMailSender javaMailSender;
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class FormSendMail {
+
+	@Value("${sender_email}")
+	String senderEmail;
+
+	@Value("${link_job}")
+	String linkJob;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 //    public  void sendMailInvite(String to,String jobId) throws MessagingException, IOException {
 //        String from = "jobfreelancer@gmail.com";
 //        String linkJob = "https://job-hunting-vn.herokuapp.com/job-details?id="+jobId;
@@ -34,24 +45,22 @@ public class FormSendMail{
 //        javaMailSender.send(msg);
 //    }
 
-    public  void sendMail(String to,String content,String jobId) throws MessagingException, IOException {
-        String from = "jobfreelancer@gmail.com";
-        String linkJob = "https://job-hunting-vn.herokuapp.com/job-details?id="+jobId;
-        MimeMessage msg = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(msg);
-        helper.setFrom(new InternetAddress(from, "TopWorkRocker Company"));
-        helper.setTo(to);
-        helper.setSubject("Suggest Apply To Job");
-        String text = "<b>Hey guy</b>,<br><i> We come from the Website <a href='https://job-hunting-vn.herokuapp.com/home'>TopWorkRocker</a></i><br><span> "+content+".</span>" +
-                "<br><a href='"+linkJob+"'>Click here<a>"+"<span> for more details.</span>"+
-                "<div style='text-align:left;'><a href='https://job-hunting-vn.herokuapp.com/home'><img src='https://res.cloudinary.com/hoadaica/image/upload/v1634898297/freelancer4-e1531398303434_vu5puk.jpg'" +
-                " alt='Rocket Team' title='Rocket Team' style='width:700px;height:400px;'></img></a></div>";
-        helper.setText(text,true);
+	public void sendMail(String to, String content, String jobId) throws MessagingException, IOException {
+		String from = this.senderEmail;
+		String linkJob = this.linkJob + jobId;
+		MimeMessage msg = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(msg);
+		helper.setFrom(new InternetAddress(from, "TopWorkRocker Company"));
+		helper.setTo(to);
+		helper.setSubject("Suggest Apply To Job");
+		String text = "<b>Hey guy</b>,<br><i> We come from the Website <a href='https://job-hunting-vn.herokuapp.com/home'>TopWorkRocker</a></i><br><span> "
+				+ content + ".</span>" + "<br><a href='" + linkJob + "'>Click here<a>"
+				+ "<span> for more details.</span>"
+				+ "<div style='text-align:left;'><a href='https://job-hunting-vn.herokuapp.com/home'><img src='https://res.cloudinary.com/hoadaica/image/upload/v1634898297/freelancer4-e1531398303434_vu5puk.jpg'"
+				+ " alt='Rocket Team' title='Rocket Team' style='width:700px;height:400px;'></img></a></div>";
+		helper.setText(text, true);
 
-
-        javaMailSender.send(msg);
-    }
-
-
+		javaMailSender.send(msg);
+	}
 
 }
