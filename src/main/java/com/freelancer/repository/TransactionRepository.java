a3353c1e7e85defe0e0e163a8d78e4e623a9c5cb
Windows 10 <trinhlhth1908021@fpt.edu.vn>
2021-10-28 22:55:42 +0700

    gegeg

--- src/main/java/com/freelancer/repository/TransactionRepository.java
+++ src/main/java/com/freelancer/repository/TransactionRepository.java
@@ -28,7 +28,7 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long>,
             ,nativeQuery=true)
     public List<Object[]> finMonth();
     @Query(value = "SELECT sum(round(price)) as sum, date(to_timestamp(create_at/1000)) as day " +
-            "from transactions where create_at BETWEEN :start AND :end  type = 3 GROUP BY day ORDER BY day"
+            "from transactions where create_at BETWEEN :start AND :end AND type = 3 GROUP BY day ORDER BY day"
             ,nativeQuery=true)
     public List<Object[]> finDay(@Param("start") Long start, @Param("end")Long end);
 }
