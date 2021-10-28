538c39b542cbeb59ddb899ad8164fa143054e19c
trinhlh96 <63459353+trinhlh96@users.noreply.github.com>
2021-10-28 18:57:47 +0700

    Trinhloca (#85)
    * thống kê

---
+++ src/main/java/com/freelancer/dto/TransactionDTO.java
@@ -0,0 +1,16 @@
+package com.freelancer.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TransactionDTO {
+    private Double price;
+    private Integer month;
+    private Date day;
+}
